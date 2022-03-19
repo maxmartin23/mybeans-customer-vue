aa564c26ebda7c1d9239837985a13df15623e4f4
Max Martin <mmart180@my.centennialcollege.ca>
2022-03-19 11:11:19 -0400

    Added auth setup component

---
+++ src/components/AuthSetup.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="display: none"></div>
+</template>
+
+<script>
+export default {
+  name: "AuthSetup",
+  computed: {
+    token() {
+      return this.$store.getters.getToken;
+    },
+  },
+  watch: {
+    token() {
+      this.setToken();
+    },
+  },
+  created() {
+    if (this.$store.getters.getToken) this.setToken();
+  },
+  methods: {
+    setToken() {
+      console.log(this.token)
+      this.$http.defaults.headers.common["Authorization"] = `${
+        this.token ? "Bearer " : ""
+      }${this.token ?? ""}`;
+    },
+  },
+};
+</script>
