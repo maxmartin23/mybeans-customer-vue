dc84d7f46ed0e1e5a46917ed36325f29db20f2ce
Max Martin <mmart180@my.centennialcollege.ca>
2022-03-19 10:57:50 -0400

    Created geolocation pages

---
+++ src/components/Home/PinLocation.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="vh-100 vw-100 position-absolute top left">
+    <v-dialog v-model="dialog" style="z-index: 100" width="32rem">
+      <v-sheet class="rounded-xl pa-6">
+        <h3>Set your location</h3>
+        <p>Do you want to set this as your location?</p>
+        <v-btn
+          color="primary"
+          rounded
+          elevation="0"
+          class="pa-6"
+          @click="setLocation"
+        >
+          Submit my location
+        </v-btn>
+      </v-sheet>
+    </v-dialog>
+    <l-map
+      id="map"
+      ref="map"
+      :zoom="zoom"
+      :min-zoom="13"
+      :center="center"
+      @click="clickHandler"
+    >
+      <l-tile-layer :url="url" :attribution="attribution" />
+    </l-map>
+  </div>
+</template>
+
+<script>
+import L from "leaflet";
+export default {
+  name: "PinLocation",
+  data() {
+    return {
+      dialog: false,
+      moving: false,
+      isDebounced: false,
+      zoom: 15,
+      center: L.latLng(this.$route.query.lat, this.$route.query.lng),
+      clickedLocation: {
+        latitude: null,
+        longitude: null,
+      },
+      url: "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVkcnNtIiwiYSI6ImNrMzhhbzFwNTAzc3EzY3RlZjV4bGVrZGYifQ.cu8dC9cP-I15VNH-JZYKGQ",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    };
+  },
+  methods: {
+    clickHandler(e) {
+      const { lat, lng } = e.latlng;
+
+      this.clickedLocation = {
+        latitude: lat,
+        longitude: lng,
+      };
+      this.dialog = true;
+    },
+    setLocation(){
+      this.$store.commit("setLatLng", this.clickedLocation);
+      this.$router.replace("/home");
+    }
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+#map {
+  height: 100%;
+  width: 100%;
+  z-index: 1
+}
+</style>
---
+++ src/components/Home/RequestLocation.vue
@@ -0,0 +1,36 @@
+<template>
+      <div
+      class="vh-100 vw-100 d-flex justify-center align-center"
+      v-if="!location"
+    >
+      <div class="text-center pa-6">
+        <h1>Where are you?</h1>
+        <p>
+          MyBeans needs to know your location to find the nearest beans to you.
+        </p>
+        <v-btn
+          color="primary"
+          @click="getLocation()"
+          rounded
+          elevation="0"
+          class="pa-6"
+          >Submit my location</v-btn
+        >
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "RequestLocation",
+  methods: {
+    getLocation() {
+      if (!navigator.geolocation) return;
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        this.$router.replace(`/pinlocation?lat=${latitude}&lng=${longitude}`);
+      });
+    },
+  },
+}
+</script>
