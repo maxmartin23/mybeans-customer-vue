0bea5252e20e75dc5cd2138cd5469570ae012143
Max Martin <mmart180@my.centennialcollege.ca>
2022-03-19 10:57:31 -0400

    Added sign up stepper component

--- src/components/Account/SignUp.vue
+++ src/components/Account/SignUp.vue
@@ -1,20 +1,22 @@
 <template>
   <v-sheet
-    class="pt-8 pa-lg-16 pa-6 rounded-xl"
+    class="pt-8 pa-lg-16 pa-6 px-0 rounded-xl"
     id="main-content"
     elevation="6"
   >
     <div class="pa-lg-16">
       <div class="text-center mb-4">
-        <my-beans-logo aria-label="MyBeans" style="width: 16rem" />
+        <my-beans-logo aria-label="MyBeans" style="width: 16rem; max-width: 100%" />
       </div>
       <h1 class="mb-4 text-center">Get started</h1>
-      <transition name="fade-shrink">
-        <v-alert dense type="error" color="error darken-2" v-if="errorMessage">
-          {{ errorMessage }}
-        </v-alert>
-      </transition>
-      <sign-up-stepper />
+       <div class="px-6">
+         <transition name="fade-shrink">
+          <v-alert dense type="error" color="error darken-2" v-if="errorMessage">
+            {{ errorMessage }}
+          </v-alert>
+               </transition>
+       </div>
+      <sign-up-stepper @error="errorMessage = $event ?? 'Something went wrong, please try again.'" />
       <div class="text-center mt-4">
         <p class="font-weight-medium grey--text text--darken-2 mb-6">
           Already have an account? click
@@ -37,6 +39,7 @@ export default {
   },
   data() {
     return {
+      errorMessage: null,
       isLoading: false,
     };
   },
