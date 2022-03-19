e51cccc29f9171340ca72f043b07d4a88b86a3ae
Max Martin <mmart180@my.centennialcollege.ca>
2022-03-19 11:11:01 -0400

    Added search and display nearby shops

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,45 @@
 <template>
-  <div></div>
+  <div>
+    <v-text-field v-model="keyword" label="Enter a keyword" />
+    <div v-for="(shop, i) in filteredShops" :key="i">
+      {{ shop?.item?.name ?? shop.name }}
+    </div>
+  </div>
 </template>
 
 <script>
+import Fuse from "fuse.js";
+
 export default {
   name: "Home",
   computed: {
     location() {
       return this.$store.getters.getLocation;
     },
+    filteredShops() {
+      if (!this.keyword) return this.shops;
+      return this.fuse.search(this.keyword);
+    },
   },
-  data(){
+  data() {
     return {
       shops: null,
-    }
+      keyword: "",
+      fuse: null,
+    };
   },
   created() {
     if (!this.location) return this.$router.replace(`/requestlocation`);
-
     this.$http
-      .get("/shops", {
+      .get(`/shops`, {
         params: {
-          lat: this.location.lat,
-          lng: this.location.lng,
+          lat: this.location.latitude,
+          lng: this.location.longitude,
         },
       })
       .then((res) => {
         this.shops = res.data;
+        this.fuse = new Fuse(this.shops, { keys: ["name", "description"] });
       })
       .catch((err) => {
         console.log(err);
