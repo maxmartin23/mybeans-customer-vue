a370c70460aff2cd8d57da0164fca9e11f14a030
Max Martin <mmart180@my.centennialcollege.ca>
2022-03-27 12:15:07 -0400

    Created shop sidebar navigation

---
+++ src/views/Shop/Nav.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav></nav>
+</template>
+
+<script>
+export default {
+  name: "ShopNav",
+};
+</script>
+
+<style lang=stylus scoped>
+nav {
+  background var(--v-primary-base);
+  position: relative;
+  width: 320px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 1rem;
+}
+
+@media(max-width: 767px){
+  nav {
+    height: 64px;
+    width: 100%;
+    position absolute;
+    bottom 0
+    left 0
+  }
+}
+</style>
