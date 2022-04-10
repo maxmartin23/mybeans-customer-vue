4b8bfb4d90162428870106b89b2d8b4bf4a14d57
Max <mmart180@my.centennialcollege.ca>
2022-04-10 16:02:05 +0800

    Added edit/delete

--- src/views/BeanDetails.vue
+++ src/views/BeanDetails.vue
@@ -48,9 +48,7 @@
               <h3 class="font-weight-medium grey--text text--darken-1">
                 {{ bean.price }} CAD
               </h3>
-              <p class="body-1">
-               Description: {{ bean.description }}
-              </p>
+              <p class="body-1">Description: {{ bean.description }}</p>
               <p>
                 Origin: {{ bean.origin }}
                 <br />
@@ -69,7 +67,7 @@
       <div v-if="reviews">
         <div
           class="blue-grey lighten-5 pa-4 rounded-lg"
-          v-if="reviews.length === 0"
+          v-if="reviews?.length === 0"
         >
           <span class="font-weight-medium">
             There are no reviews yet for {{ bean.name }}
@@ -81,21 +79,40 @@
           rounded
           class="py-6 my-4"
           @click="reviewDialog = true"
-          v-if="!reviews.find(review => review.user.userId === $store.getters.getUser.userId)"
+          v-if="
+            !reviews.find(
+              (review) => review.user.userId === $store.getters.getUser.userId
+            )
+          "
         >
           <v-icon left>mdi-plus</v-icon> Submit a review
         </v-btn>
-        <div class="pb-8" v-if="reviews.length > 0">
+        <div class="pb-8" v-if="reviews?.length > 0">
           <h2 class="mb-6">Reviews for {{ bean.name }}</h2>
           <div class="py-4" v-for="(review, i) in reviews" :key="i">
-            <p class="text-h6 mb-0">
-              {{review.user.firstName}} {{review.user.lastName}}
-            </p>
-            <v-rating class="ml-n2" small  :value="review.rating" readonly />
-            <p class="body-1">{{review.comment}}</p>
-            <small class="grey--text text--darken-2">
-              {{review.createdAt}}
-            </small>
+            <div class="d-flex">
+              <div class="flex-grow-1">
+                <p class="text-h6 mb-0">
+                  {{ review.user.firstName }} {{ review.user.lastName }}
+                </p>
+                <v-rating class="ml-n2" small :value="review.rating" readonly />
+                <p class="body-1">{{ review.comment }}</p>
+                <small class="grey--text text--darken-2">
+                  {{ review.createdAt }}
+                </small>
+              </div>
+              <div class="d-flex align-center" v-if="review.user.userId === $store.getters.getUser.userId">
+                <a href="#" class="mr-4" @click.prevent="editReviewDialog()"
+                  >Edit</a
+                >
+                <a
+                  href="#"
+                  class="grey--text text--darken-2"
+                  @click="deleteReview()"
+                  >Delete</a
+                >
+              </div>
+            </div>
             <v-divider></v-divider>
           </div>
         </div>
@@ -149,7 +166,26 @@ export default {
           alert("Error loading bean reviews. Please try again.");
         });
     },
+    editReviewDialog() {
+      const review = this.reviews.find(
+        (review) => review.user.userId === this.$store.getters.getUser.userId
+      );
+      if (!review) {
+        return;
+      }
+      this.newReview.rating = review.rating;
+      this.newReview.comment = review.comment;
+      this.reviewDialog = true;
+    },
     submitReview() {
+      //Check if user has already submitted a review
+      const review = this.reviews.find(
+        (review) => review.user.userId === this.$store.getters.getUser.userId
+      );
+      if (review) {
+        return this.editReview();
+      }
+
       const { coffeeBeanId } = this.bean;
       const { rating, comment } = this.newReview;
       this.$http
@@ -158,6 +194,57 @@ export default {
           alert("Review submitted successfully.");
           this.reviewDialog = false;
           this.reviews.unshift(res.data);
+        })
+        .catch((err) => {
+          const error =
+            err.response?.data?.error ??
+            "Something went wrong, please try again.";
+          alert(error);
+        });
+    },
+    editReview() {
+      const { coffeeBeanId } = this.bean;
+      const { rating, comment } = this.newReview;
+      this.$http
+        .post("/reviews/update", { coffeeBeanId, rating, comment })
+        .then(() => {
+          alert("Review edited successfully.");
+          this.reviewDialog = false;
+          const review = this.reviews.find(
+            (review) =>
+              review.user.userId === this.$store.getters.getUser.userId
+          );
+          review.rating = rating;
+          review.comment = comment;
+        })
+        .catch((err) => {
+          const error =
+            err.response?.data?.error ??
+            "Something went wrong, please try again.";
+          alert(error);
+        });
+    },
+    deleteReview() {
+      const confirm = window.confirm(
+        "Are you sure you want to delete this review?"
+      );
+      if (!confirm) {
+        return;
+      }
+      const review = this.reviews.find(
+        (review) => review.user.userId === this.$store.getters.getUser.userId
+      );
+      if (!review) {
+        return;
+      }
+      const { coffeeBeanId } = this.bean;
+      this.$http
+        .delete(`/reviews/delete?id=${coffeeBeanId}`)
+        .then(() => {
+          alert("Review deleted successfully.");
+          this.reviews = this.reviews.filter(
+            (review) => review.userId !== this.$store.getters.getUser.userId
+          );
         })
         .catch((err) => {
           const error =
